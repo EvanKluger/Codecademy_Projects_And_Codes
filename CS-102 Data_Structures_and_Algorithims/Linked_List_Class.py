eac4577de1f455e2bd5b7ed102c26aaffc4edb62
Evan Kluger <108431972+EvanKluger@users.noreply.github.com>
2022-07-26 10:53:20 -0400

    Update Linked_List_Class.py

--- CS-102 Data_Structures_and_Algorithims/Linked_List_Class.py
+++ CS-102 Data_Structures_and_Algorithims/Linked_List_Class.py
@@ -1,16 +1,5 @@
-class Node:
-  def __init__(self, value, next_node=None):
-    self.value = value
-    self.next_node = next_node
-    
-  def get_value(self):
-    return self.value
-  
-  def get_next_node(self):
-    return self.next_node
-  
-  def set_next_node(self, next_node):
-    self.next_node = next_node
+from Node_Class import Node
+
 
 class LinkedList:
   def __init__(self, value=None):

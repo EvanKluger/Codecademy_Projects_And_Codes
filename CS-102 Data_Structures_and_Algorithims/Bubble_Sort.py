d64591f82fb600486f10464d0dae92ae443cb13e
Evan Kluger <108431972+EvanKluger@users.noreply.github.com>
2022-07-26 11:23:42 -0400

    Create Bubble_Sort.py

---
+++ CS-102 Data_Structures_and_Algorithims/Bubble_Sort.py
@@ -0,0 +1,21 @@
+nums = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+print("PRE SORT: {0}".format(nums))
+
+def swap(arr, index_1, index_2):
+  temp = arr[index_1]
+  arr[index_1] = arr[index_2]
+  arr[index_2] = temp
+
+def bubble_sort(arr):
+  iteration_count = 0
+  for i in range(len(arr)):
+    # iterate through unplaced elements
+    for idx in range(len(arr) - i - 1):
+      iteration_count += 1
+      if arr[idx] > arr[idx + 1]:
+        # replacement for swap function
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+        
+  
+bubble_sort(nums)
+print("POST SORT: {0}".format(nums))
